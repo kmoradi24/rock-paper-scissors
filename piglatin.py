50584bf88c88d97f388ae21415c21b45453a7dab
kmoradi24 <44955773+kmoradi24@users.noreply.github.com>
2018-11-14 13:32:18 -0800

    Update piglatin.py

--- piglatin.py
+++ piglatin.py
@@ -1,15 +1,10 @@
-pyg = 'ay'
-original_response = raw_input ("Which word would you like translated?")
-word = original_response.lower()
-if len(word) > 0:
-  print word
-else:
-  print "this isn't a word"
-p = word[0]
-new_word = word + p + pyg
-new_word = new_word[1:len(new_word)]
-
+  pyg = 'ay'
+original_response = raw_input("Which word would you like translated?")
 if len(original_response) > 0 and original_response.isalpha():
+  word = original_response.lower()
+  p = word[0]
+  new_word = word + p + pyg
+  new_word = new_word[1:len(new_word)]
   print new_word
 else:
-  print "this isn't a word"
+    print "this isn't a word"
